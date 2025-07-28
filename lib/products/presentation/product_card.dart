bd6a8dac55c305be5ba36dd57e849052bd4693f5
Aakash Mehta <akashmehta4444@gmail.com>
2025-07-28 09:58:42 +0530

    updated ui

--- lib/products/presentation/product_card.dart
+++ lib/products/presentation/product_card.dart
@@ -1,104 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:awesome_rating/awesome_rating.dart';
 
 class ProductCard extends StatelessWidget {
   final Map<String, dynamic> product;
+
   const ProductCard({super.key, required this.product});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.all(16),
+      margin: const EdgeInsets.all(6),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       elevation: 4,
       child: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(6),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Top row: Rating and image
-            Row(
-              children: [
-                Expanded(flex: 1, child: CircleAvatar(
-                  backgroundColor: Colors.green,
-                  radius: 24,
-                  child: Text(
-                    product['rating'].toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                )),
-                const SizedBox(width: 12),
-                Expanded(flex: 5, child: Column(children: [
-                  Text(
-                    product['title'],
-                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const SizedBox(height: 6,),
-                  Text(
-                    product['description'] ?? '',
-                    style: const TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],)),
-
-                const SizedBox(width: 12),
-                // Replace with actual asset or network image
-                Expanded(flex: 3, child: Image.network(
-                  product['images'][0],
-                  fit: BoxFit.cover,
-                )),
-              ],
-            ),
-            const SizedBox(height: 12),
-            // Discount label
-            Row(
-              children: [
-                Text(product['brand'] ?? 'Unknown'),
-                const SizedBox(width: 8),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  decoration: BoxDecoration(
-                    color: Colors.orangeAccent,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: const Text(
-                    '7% OFF',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            // Buttons
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                  ),
-                  child: const Text('More info'),
-                ),
-                const SizedBox(width: 16),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Compare prices'),
-                ),
-              ],
-            ),
+            // Top row: Title, Description, and Image
+            imageView(product),
+            productDetailView(product),
+            buttonView(product),
           ],
         ),
       ),
     );
   }
+
+  Widget imageView(Map<String, dynamic> product) => Container(
+    padding: const EdgeInsets.all(6),
+    width: double.infinity,
+    alignment: Alignment.topCenter,
+    child: CachedNetworkImage(
+      imageUrl: product['images'][0],
+      placeholder: (context, url) =>
+          Image.asset('assets/placeholder_product.webp'),
+      errorWidget: (context, error, stackTrace) => Icon(Icons.error),
+      fit: BoxFit.cover,
+    ),
+  );
+
+  Widget productDetailView(Map<String, dynamic> product) => Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        product['title'],
+        style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      const SizedBox(height: 6),
+      Text(
+        product['description'] ?? '',
+        style: const TextStyle(fontSize: 12, fontWeight: FontWeight.normal),
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+      ),
+      const SizedBox(height: 10),
+
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 5,
+            child: Text(
+              textAlign: TextAlign.start,
+              'Rs. ${product['price'] ?? 'NA'}',
+              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: IgnorePointer(
+              child: AwesomeStarRating(
+                rating: product['rating'],
+                starCount: 5,
+                allowHalfRating: true,
+                size: 15,
+              ),
+            ),
+          ),
+        ],
+      ),
+      const SizedBox(height: 10),
+    ],
+  );
+
+  Widget buttonView(Map<String, dynamic> product) => ElevatedButton(
+    onPressed: () {},
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.green,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+    ),
+    child: Text(
+      'Add To Cart',
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 12,
+        fontWeight: FontWeight.bold,
+      ),
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+    ),
+  );
 }
--- lib/products/presentation/product_screen.dart
+++ lib/products/presentation/product_screen.dart
@@ -12,24 +12,35 @@ class ProductListScreen extends ConsumerWidget {
     final productState = ref.watch(productListNotifierProvider);
 
     return Scaffold(
-      appBar: AppBar(title: Text("Product List"),),
+      appBar: AppBar(title: Text("Product List")),
       body: productState.when(
         data: (data) => NotificationListener<ScrollNotification>(
-            onNotification: (scrollInfo) {
-              if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent &&
-                  notifier.hasMore && !notifier.isLoading) {
-                print('ProductList data : ${data.length}');
-                notifier.fetchNextPage();
-              }
-              return false;
-            },
-            child: ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) => ProductCard(product: data[index]),
-            ),
+          onNotification: (scrollInfo) {
+            if (scrollInfo.metrics.pixels ==
+                    scrollInfo.metrics.maxScrollExtent &&
+                notifier.hasMore &&
+                !notifier.isLoading) {
+              print('ProductList data : ${data.length}');
+              notifier.fetchNextPage();
+            }
+            return false;
+          },
+          child: GridView.count(
+            crossAxisCount: 2,
+            // Number of columns
+            crossAxisSpacing: 0,
+            // Horizontal spacing between items
+            mainAxisSpacing: 0,
+            childAspectRatio: 0.55,
+            // Vertical spacing between items
+            padding: EdgeInsets.all(6),
+            children: List.generate(data.length, (index) {
+              return ProductCard(product: data[index]);
+            }),
           ),
-        error: (e, _) => Center(child: Text('Error : $e'),),
-        loading: () => Center(child: CircularProgressIndicator(),)
+        ),
+        error: (e, _) => Center(child: Text('Error : $e')),
+        loading: () => Center(child: CircularProgressIndicator()),
       ),
     );
   }

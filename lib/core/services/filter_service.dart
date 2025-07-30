caf61960ad888dd193d70e5d6ab1e952eab95cc1
Aakash Mehta <akashmehta4444@gmail.com>
2025-07-30 09:57:06 +0530

    refactored filter ranges.

--- lib/core/services/filter_service.dart
+++ lib/core/services/filter_service.dart
@@ -12,11 +12,11 @@ class FilterService {
         filterItemsMap[value] = products.map(
                 (product) {return product.brand;}).toSet().toList();
       } else if (index == 2) {
-        filterItemsMap[value] = ['0 - 100', '100 - 200', '200 - 300', '300 - 400', '400 - 500'];
+        filterItemsMap[value] = ['0 - 500', '501 - 1000', '1001 - 2000', '>2000'];
       } else if (index == 3) {
         filterItemsMap[value] = ['1', '2', '3', '4', '5'];
       } else if (index == 4) {
-        filterItemsMap[value] = ['0 - 100', '100 - 200', '200 - 300', '300 - 400', '400 - 500'];
+        filterItemsMap[value] = ['0 - 5', '6 - 10', '11 - 15', '16 - 20', '>20'];
       }
     });
     return filterItemsMap;
